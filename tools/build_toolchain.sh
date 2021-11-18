30290d72d46d2552ed9a4a87adfd77259238a7ac
deuskane <deuskane@gmail.com>
2021-11-18 15:28:21 +0100

    rename buid.sh script

---
+++ tools/build_toolchain.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------
+# Title      : Build script
+# Project    : OpenBlaze8
+#-----------------------------------------------------------------------------
+# File       : build_toolchain.sh
+# Author     : mrosiere
+#-----------------------------------------------------------------------------
+# Description: 
+#-----------------------------------------------------------------------------
+# Copyright (c) 2021
+#-----------------------------------------------------------------------------
+# Revisions  :
+# Date        Version  Author   Description
+# 2021-10-26  1.0      mrosiere Created
+# 2021-11-03  1.1      mrosiere Use generator
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+# build_toolchain_sdcc
+#-----------------------------------------------------------------------------
+# Argument 1 : SDCC source  directory
+# Argument 2 : SDCC install directory
+#-----------------------------------------------------------------------------
+# Build SDCC tool
+#-----------------------------------------------------------------------------
+function build_toolchain_sdcc()
+{
+    local dir_src=$1
+    local dir_install=$2
+    local dir_build=${dir_src}/build.sdcc
+
+    mkdir -p ${dir_build}
+    cd       ${dir_build}
+
+    chmod +x ${dir_src}/configure
+    chmod +x ${dir_src}/sim/ucsim/mkecho
+
+    echo "[SDCC] Configure"
+    ${dir_src}/configure \
+	--prefix=${dir_install} \
+	--disable-{pic14,pic16,hc08,stm8,ds390,ds400,z80,mcs51,s08,z180,r2k,r3ka,gbz80,tlcs90}-port \
+	--disable-sdbinutils  \
+	--disable-device-lib  \
+	--disable-ucsim       \
+	--disable-sdcdb       \
+	--disable-packihx
+
+    echo "[SDCC] Build"
+    make -j
+
+    echo "[SDCC] Install"
+    make -j install
+
+    cd -
+}
+
+#-----------------------------------------------------------------------------
+# build_toolchain_picoasm
+#-----------------------------------------------------------------------------
+# Argument 1 : PICOASM Source directory
+# Argument 2 : PICOASM install directory
+#-----------------------------------------------------------------------------
+# Build PICOASM tool
+#-----------------------------------------------------------------------------
+function build_toolchain_picoasm()
+{
+    local dir_src=$1
+    local dir_install=$2
+
+    echo "[PICOASM] Build"
+    make -C ${dir_src} -j
+
+    echo "[PICOASM] Install"
+    mkdir -p   ${dir_install}/bin
+    mkdir -p   ${dir_install}/share/picoasm/xilinx
+    mkdir -p   ${dir_install}/share/picoasm/generic
+
+    cp ${dir_src}/picoasm ${dir_install}/bin
+
+    cp ${dir_src}/ROM_form.v      ${dir_install}/share/picoasm/xilinx
+    cp ${dir_src}/ROM_form.vhd    ${dir_install}/share/picoasm/xilinx
+    cp ${dir_src}/../ROM_form.vhd ${dir_install}/share/picoasm/generic
+
+}
+
+#-----------------------------------------------------------------------------
+# build_toolchain_main
+#-----------------------------------------------------------------------------
+# Argument 1 : ROM file (VHDL file)
+# Argument 2 : test name
+# Argument 3 : Target name
+#-----------------------------------------------------------------------------
+# Generate all tools and ROM
+#-----------------------------------------------------------------------------
+function build_toolchain_main()
+{
+    BUILD_TOOLCHAIN_SDCC=false
+    BUILD_TOOLCHAIN_PICOASM=false
+
+    # Directory
+    dir_tools=`/bin/pwd`
+    dir_ip=${dir_tools}/..
+    dir_picoasm=${dir_tools}/picoasm
+    dir_sdcc=${dir_tools}/pbccv2-src-20110901/sdcc3
+    dir_install=${dir_tools}/install
+    
+    # SDCC Compilation
+    if ${BUILD_TOOLCHAIN_SDCC};
+    then
+	build_toolchain_sdcc    ${dir_sdcc} ${dir_install}
+    fi
+
+    # Picoasm Compilation
+    if ${BUILD_TOOLCHAIN_PICOASM};
+    then
+	build_toolchain_picoasm ${dir_picoasm} ${dir_install}
+    fi
+
+}
+
+build_toolchain_main $*
