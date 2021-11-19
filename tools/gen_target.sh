79dd1a1ae913390c569ebae2cc51cbd7788a9ea5
deuskane <deuskane@gmail.com>
2021-11-19 10:57:53 +0100

    gen_target.sh : add options

--- tools/gen_target.sh
+++ tools/gen_target.sh
@@ -15,6 +15,7 @@
 # Date        Version  Author   Description
 # 2021-10-26  1.0      mrosiere Created
 # 2021-11-03  1.1      mrosiere Use generator
+# 2021-11-19  1.2      mrosiere Add options
 #-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
@@ -46,7 +47,7 @@ EOT
 	cat <<EOT >> /tmp/test_core_targets.txt
   sim_${target_name}:
     << : *sim
-    description     : Simulation tof the test ${file_name}
+    description     : Simulation of the test ${file_name}
     filesets_append :
       - files_sim
       - pbcc_dep
@@ -60,27 +61,99 @@ EOT
 }
 
 #-----------------------------------------------------------------------------
-# gen_target_main
+# build_toolchain_usage
 #-----------------------------------------------------------------------------
-# Argument 1 : Core File
-# Argument 2 : Directory with asm (pblazeide) source code
-# Argument 3 : Directory with c               source code
+# Display usage message
+#-----------------------------------------------------------------------------
+function gen_target_usage()
+{
+    echo "$0 usage:"
+    grep " .)\ ##" $0
+
+    echo ""
+    echo "Update core file and create Makefile"
+    echo "ASM files have the psm extension. The \"pblazeide\" asm files and \"kcpsm3\" asm files must be in separate directory"
+    echo "C   files have the c   extension."
+    echo "All sources files must be single file".
+}
+
+#-----------------------------------------------------------------------------
+# gen_target_main
 #-----------------------------------------------------------------------------
 # Generate all tools and ROM
 #-----------------------------------------------------------------------------
 function gen_target_main()
 {
+    # Chek argument
+    echo "Check arguments"
+    while getopts ":p:k:c:f:h" arg; do
+	case $arg in
+	    f) ## Core File (mandatory)
+		file_core=${OPTARG}
+		if test ! -f "${file_core}";
+		then
+		    echo "Invalid file core : ${file_core}";
+		    gen_target_usage
+		    exit -1;
+		fi
+		;;
+	    p) ## ASM pblazeide source directory
+		dir_asm_pblazeide=${OPTARG}
+
+		if test ! -d "${dir_asm_pblazeide}";
+		then
+		    echo "Invalid directory : ${dir_asm_pblazeide}";
+		    gen_target_usage
+		    exit -1;
+		fi
+
+		;;
+	    k) ## ASM kcpsm3 source directory
+		dir_asm_kcpsm3=${OPTARG}
+
+		if test ! -d "${dir_asm_kcpsm3}";
+		then
+		    echo "Invalid directory : ${dir_asm_kcpsm3}";
+		    gen_target_usage
+		    exit -1;
+		fi
+
+		;;
+	    c) ## C source directory
+		dir_c=${OPTARG}
+
+
+		if test ! -d "${dir_c}";
+		then
+		    echo "Invalid directory : ${dir_c}";
+		    gen_target_usage
+		    exit -1;
+		fi
+
+		;;
+	    h | *) ## Display help
+		echo "${OPTARG}"
+		gen_target_usage
+		exit 0
+		;;
+	esac
+    done
+
+    # Check Arguments
+    if test -z "${file_core}";
+    then
+	echo "File Core must be define";
+	gen_target_usage
+	exit -1;
+    fi
+    
     # Directory
-    dir_ip=`/bin/pwd`
-    dir_test_asm=${2}
-    dir_test_c=${3}
+    dir_ip=`dirname ${file_core}`
 
     # File
-    file_core=${1}
     file_make=${dir_ip}/mk/targets.mk
-    
-    mkdir -p ${dir_ip}/mk
-    	
+
+    # Create tmp file
     rm -f /tmp/test_core_filesets.txt
     rm -f /tmp/test_core_generate.txt
     rm -f /tmp/test_core_targets.txt
@@ -91,28 +164,58 @@ function gen_target_main()
     touch /tmp/test_core_targets.txt
     touch /tmp/test_make.txt
 
+    # Scan source file
     dir_ip=${dir_ip}/
-    for f in `find ${dir_test_asm}/*.psm`; do
 
-	file_name=${f/${dir_ip}/}
-	test_name=`basename ${f/.psm/}`
-	target_name=asm_${test_name}
-	
-	gen_target_add_target ${file_name} "pblazeide" ${target_name}
-	
-    done
+    echo "Scan source directory"
+    if test -n "${dir_asm_pblazeide}";
+    then
+	echo " * ASM (pblazeide)"
+	for f in `find ${dir_asm_pblazeide}/*.psm`; do
+	    echo "   * ${f}" 
 
-    for f in `find ${dir_test_c}/*.c`; do
+	    file_name=${f/${dir_ip}/}
+	    test_name=`basename ${f/.psm/}`
+	    target_name=asm_${test_name}
 
-	file_name=${f/${dir_ip}/}
-	test_name=`basename ${f/.c/}`
-	target_name=c_${test_name}
+	    gen_target_add_target ${file_name} "pblazeide" ${target_name}
+	    
+	done
+    fi
+    
+    if test -n "${dir_asm_kcpsm3}";
+    then
+	echo " * ASM (kcpsm3)"
+	for f in `find ${dir_asm_kcpsm3}/*.psm`; do
+	    echo "   * ${f}" 
 
-	gen_target_add_target ${file_name} "c" ${target_name}
-	
-    done
+	    file_name=${f/${dir_ip}/}
+	    test_name=`basename ${f/.psm/}`
+	    target_name=asm_${test_name}
+	    
+	    gen_target_add_target ${file_name} "kcpsm3" ${target_name}
+	    
+	done
+    fi
+    
+    if test -n "${dir_c}";
+    then
+	echo " * C"
+	for f in `find ${dir_c}/*.c`; do
+	    echo "   * ${f}" 
+
+	    file_name=${f/${dir_ip}/}
+	    test_name=`basename ${f/.c/}`
+	    target_name=c_${test_name}
+
+	    gen_target_add_target ${file_name} "c" ${target_name}
+	    
+	done
+    fi
 
     # Edit Core File
+    echo "Edit Core File"
+    
     sed -i '/<FILESETS_BEGIN>/,/<FILESETS_END>/{/<FILESETS_BEGIN>/!{/<FILESETS_END>/!d}}' ${file_core}
     sed -i '/<GENERATE_BEGIN>/,/<GENERATE_END>/{/<GENERATE_BEGIN>/!{/<GENERATE_END>/!d}}' ${file_core}
     sed -i '/<TARGETS_BEGIN>/,/<TARGETS_END>/{/<TARGETS_BEGIN>/!{/<TARGETS_END>/!d}}'     ${file_core}
@@ -121,10 +224,12 @@ function gen_target_main()
     sed -i '/<GENERATE_BEGIN>/r /tmp/test_core_generate.txt'                              ${file_core}
     sed -i '/<TARGETS_BEGIN>/r  /tmp/test_core_targets.txt'                               ${file_core}
 
-    # Edit Targets
+    # Generate Makefile
+    echo "Generate Makefile"
+    mkdir -p ${dir_ip}/mk
+    	
     cp /tmp/test_make.txt ${file_make}
 
-    # Generate Makefile
     rm -f Makefile
     cat <<EOT >> ${dir_ip}/Makefile
 #-----------------------------------------------------------------------------
@@ -174,6 +279,8 @@ clean	:
 
 .PHONY	: clean
 EOT
+
+    echo "Done"
 
 }
 
